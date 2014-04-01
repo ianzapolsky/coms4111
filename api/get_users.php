41c5606de6341d88e9384b52ecb5db38136442c2
Ian Zapolsky <iaz2105@columbia.edu>
2014-04-01 12:45:38 -0400

    This is the first real working example of integrating the beginnings of our php API into the front end. Here we've used jQuery and its builtin .ajax() method to ping our database for usernames, and then we've displayed all the usernames in sb_index.html. I'm beginning to doubt whether the complexity of this assignment really calls for a javascript framework. We could probably just do the whole thing with jquery and call it good.

--- api/get_users.php
+++ api/get_users.php
@@ -4,13 +4,10 @@ $conn = oci_connect('iaz2105', 'a', 'w4111b.cs.columbia.edu/ADB');
 
 if (!$conn) {
   echo '<p>Database not connected!</p>';
-} else {
-  echo '<p>Connected successfully!</p>';
 }
 
 // see if we are querying a specific user
 if ($_GET['name']) {
-  echo 'argument present in the form of ' . $_GET['name'];
   // prepare statement
   $msg = "SELECT * FROM users WHERE username='" . $_GET['name'] . "'";
   $stid = oci_parse($conn, $msg); 
@@ -34,21 +31,10 @@ if (!$r) {
 
 // fetch results of entire query at once
 $rows = oci_fetch_all($stid, $result);
-echo '<p>' . $rows . ' rows fetched</p>';
 
 // convert to json and print
-echo json_encode($result);
-
-/* Fetch the results of the query
-print "<table border='1'>\n";
-while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
-    print "<tr>\n";
-    foreach ($row as $item) {
-        print "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
-    }
-    print "</tr>\n";
-}
-print "</table>\n";*/
+$json = json_encode($result);
+echo $json;
 
 oci_free_statement($stid);
 oci_close($conn);
