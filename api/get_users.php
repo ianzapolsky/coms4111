c5540e61052f3f20bf531b40ab8c33381c90af09
Ian Zapolsky <iaz2105@columbia.edu>
2014-04-01 03:41:20 -0400

    added bootstrab libs and some intial backend php, specifically three files to return json representations of users, schedules, and commitments, based on GET request variables

--- api/get_users.php
+++ api/get_users.php
@@ -3,26 +3,38 @@
 $conn = oci_connect('iaz2105', 'a', 'w4111b.cs.columbia.edu/ADB');
 
 if (!$conn) {
-  echo '<p>' . oci_error() . '</p>';
+  echo '<p>Database not connected!</p>';
 } else {
   echo '<p>Connected successfully!</p>';
 }
 
-// prepare the statement
-$stid = oci_parse($conn, 'SELECT * FROM users');
-if (!$stid) {
-    echo '<p>' . oci_error($conn) . '</p>';
+// see if we are querying a specific user
+if ($_GET['name']) {
+  echo 'argument present in the form of ' . $_GET['name'];
+  // prepare statement
+  $msg = "SELECT * FROM users WHERE username='" . $_GET['name'] . "'";
+  $stid = oci_parse($conn, $msg); 
+  if (!$stid) {
+    echo '<p>Statement preparation failed!</p>';
+  }
+}
+else {
+  // prepare the statement
+  $stid = oci_parse($conn, 'SELECT * FROM users');
+  if (!$stid) {
+    echo '<p>Statement preparation failed!</p>';
+  }
 }
 
 // perform the logic of the query
 $r = oci_execute($stid);
 if (!$r) {
-    echo '<p>' . oci_error($stid) . '</p>';
+    echo '<p>Logic performance failed!</p>';
 }
 
 // fetch results of entire query at once
 $rows = oci_fetch_all($stid, $result);
-echo '$rows rows fetched<br>\n';
+echo '<p>' . $rows . ' rows fetched</p>';
 
 // convert to json and print
 echo json_encode($result);
