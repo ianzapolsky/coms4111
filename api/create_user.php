72350521f1cadda7c5ad510ea09560b01fb60181
Ian Zapolsky <iaz2105@columbia.edu>
2014-04-02 10:41:46 -0400

    Added create_user.php, which allows us to directly add a user record to the database using POST data from an HTML form sent with AJAX/javascript. At this point we've pretty much done all the dirty work to make our project work. We can get any information we want, and we can post any information we want in the form of a JSON object.
    Now, we can design our frontend in earnest. The most complicated part of
    this will be the calendar, which we should begin looking into immediately.

---
+++ api/create_user.php
@@ -0,0 +1,40 @@
+<?php 
+  
+/**
+ * create_user.php
+ * create a new user from a username and password passed as POST variables from
+ * some form. 
+ */
+
+// initialize database connection
+$conn = oci_connect('iaz2105', 'a', 'w4111b.cs.columbia.edu/ADB');
+if (!$conn) {
+  die('Database not connected!');
+}
+
+if ($_POST['USERNAME'] && $_POST['PASSWORD']) { 
+  $username = $_POST['USERNAME'];
+  $password = $_POST['PASSWORD'];
+}
+else {
+  die('Incomplete data!');
+}
+
+// prepare statement
+$msg = "insert into users values ('" . $username . "', '" . $password . "')";
+$stid = oci_parse($conn, $msg); 
+if (!$stid) {
+  die('Statement preparation failed!');
+}
+
+// perform the logic of the query
+$r = oci_execute($stid);
+if (!$r) {
+  die('Logic performance failed!');
+}
+
+// free resources related to Oracle cursor, end connection to database
+oci_free_statement($stid);
+oci_close($conn);
+
+?>
