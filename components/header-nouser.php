f71faef9f2b921d5b89b942bb2cecb89505ae82b
Shensi Ding <sd2699@columbia.edu>
2014-04-05 17:39:30 -0400

    Modified so that index changes the layout and javascript code depending on whether user is logged in and removed unnecessary pages.

--- components/header-nouser.php
+++ components/header-nouser.php
@@ -20,6 +20,12 @@
 
     <!-- js -->
     <script src="js/libs/jquery-1.11.0.min.js" type="text/javascript"></script>
+    <script src="js/sb_login.js" type="text/javascript"></script>
+    <script>
+      $(document).ready(function () {
+    sb_login.init($( '#login-form' ))
+         });
+    </script>
 
   </head>
 
@@ -47,25 +53,6 @@
 	        <li><a href="#contact">Contact</a></li>
           </ul>
 
-          <ul class="nav navbar-nav navbar-right">
-
-          <form class="navbar-form" role="form" id="create-user">
-<li style="display: inline-block;">
-            <div class="form-group">
-              <input type="text" placeholder="Username" class="form-control" id="username" name="username">
-            </div>
-</li>
-<li style="display: inline-block;">
-            <div class="form-group">
-	          <input type="password" placeholder="Password" class="form-control" id="password" name="password">
-            </div>
-</li>
-<li style="display: inline-block;">
-            <button type="submit" class="btn btn-default">Sign in</button>
-</li>
-          </form>
-
-	    </li></ul>
         </div><!--/.nav-collapse -->
       </div>
     </div>
--- index.php
+++ index.php
@@ -1,67 +1,13 @@
 <?php session_start(); ?>
 
 <?php if (empty($_SESSION['username'])): ?>
-  <html>
-    <head>
-    <title>Schedule Buddy</title>
-    <link href="css/libs/bootstrap.min.css" rel="stylesheet">
-    <script src="js/libs/jquery-1.11.0.min.js" type="text/javascript"></script>
-    <script src="js/sb_login.js" type="text/javascript"></script>
-    </head>
-    <body>
-
-    <!-- LOGIN FORM -->
-    <div class="container">
-      <div class="text-centered">
-        <h1>ScheduleBuddy Login</h1>
-        <form id="login-form" action="sb_login.php" method="post">
-          <div class="form-group" style="width:400px;">
-            <p id="error"></p>
-            <p>username: <input id="username" class="form-control" type="text" name="username"></p>
-            <p>password: <input id="password" class="form-control" type="password" name="password"></p>
-            <button class="btn btn-primary" type="submit">login</button>
-          </form>
-        </div>
-      </div>
-    </div>
-    </body>
-  </html>
-
-  <script>
-    $(document).ready(function () {
-      sb_login.init($( '#login-form' ))
-    });
-  </script>
-
-<?php else: ?>
-  <html>
-    <head>
-    <title>Schedule Buddy</title>
-    <link href="css/libs/bootstrap.min.css" rel="stylesheet">
-    <script src="js/libs/jquery-1.11.0.min.js" type="text/javascript"></script>
-    </head>
-    <body>
-    
-      <div class="container">
-     
-        <div class="text-centered">
-          <h1>ScheduleBuddy</h1>
-          <h3>Welcome, <?php echo $_SESSION['username']; ?>!</h3>
-          <h3><a href="sb_other.php">test link</a></h3>
-
-          <form action="sb_logout.php">
-            <div class="form-group" style="width:400px;">
-              <button class="btn btn-danger" type="submit">logout</button>
-            </div>
-          </form>
-
-        </div>
-
-      </div>
-
-    </body>
+<!-- PAGE WITH LOGIN FORM -->
+<?php include ('sb_nouser.php'); ?>
 
     <script>
+    $(document).ready(function () {
+    sb_login.init($( '#login-form' ))
+       });
 
     $( '#create-user' ).submit(function(event) {
       event.preventDefault();
@@ -81,7 +27,14 @@
       location.reload();
     });
   </script>
+  
+<?php else: ?>
+<!-- PAGE WITH NO LOGIN FORM -->
+<?php include ('sb_user.php'); ?>
 
-</html>
 
 <?php endif; ?>
+
+
+
+
