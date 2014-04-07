8490767f1a3da449bc8569ee73db9b8b9155cb89
Ian Zapolsky <iaz2105@columbia.edu>
2014-04-07 17:47:23 -0400

    added missing header-nouser file

---
+++ components/header-nouser.php
@@ -0,0 +1,54 @@
+der if there is no username -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <title>ScheduleBuddy</title>
+    
+    <!-- Bootstrap css -->
+    <link href="css/libs/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/libs/navbar-fixed-top.css" rel="stylesheet">
+    <link href="css/libs/sticky-footer.css" rel="stylesheet">
+
+    <!-- js -->
+    <script src="js/libs/jquery-1.11.0.min.js" type="text/javascript"></script>
+    <script src="js/sb_login.js" type="text/javascript"></script>
+
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">ScheduleBuddy</a>
+        </div>
+
+        <!-- Navigation Here -->
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
