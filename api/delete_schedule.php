0486b6eef38ba5e477656e0c7491ebc08fddcff8
Ian Zapolsky <iaz2105@columbia.edu>
2014-04-10 01:00:27 -0400

    made changes to javascript in order to bring things together

---
+++ api/delete_schedule.php
@@ -0,0 +1,38 @@
+<?php 
+  
+/**
+ * delete_schedule.php
+ * delete a row from the schedules table, destroying the sched whose sid and 
+ * are passed as a GET URL parameter
+ */
+
+// initialize database connection
+$conn = oci_connect('iaz2105', 'a', 'w4111b.cs.columbia.edu/ADB');
+if (!$conn) {
+  die('Database not connected!');
+}
+
+// check for username
+if (!$_GET['sid']) 
+  die('sid was not provided!');
+}
+else {
+  // prepare statement
+  $msg = "delete from schedules where sid='" . $_GET['sid'] . "'";
+  $stid = oci_parse($conn, $msg); 
+  if (!$stid) {
+    die('Statement preparation failed!');
+  }
+}
+
+// perform the logic of the query
+$r = oci_execute($stid);
+if (!$r) {
+  die('Logic performance failed!');
+}
+
+// free resources related to Oracle cursor, end connection to database
+oci_free_statement($stid);
+oci_close($conn);
+
+?>
